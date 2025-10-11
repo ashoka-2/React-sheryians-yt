a83725cadf9ec7b22ad6957980d7e625737bb77a
Ashok Kumar <choudharyashok1230@gmail.com>
2025-10-11 11:19:09 +0530

    firstprogram Learning React part 1

---
+++ firstproject/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <div id='papa'>papaji</div>
+      <div id='chacha'>chachaji</div>
+    </>
+    
+  )
+}
+
+export default App
