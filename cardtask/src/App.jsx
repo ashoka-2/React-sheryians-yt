c50f98dc341ea4eb31c6144c075d375eef19ef27
Ashok Kumar <choudharyashok1230@gmail.com>
2025-12-25 11:26:18 +0530

    added button clicks

--- cardtask/src/App.jsx
+++ cardtask/src/App.jsx
@@ -4,9 +4,11 @@ import Users from './components/Users'
 const App = () => {
   
   return (
-    <div className='w-full h-full  p-5 flex flex-wrap gap-5 justify-center'>
-      {Users.map((user)=>(
-          <Card image={user.avatar} name={user.name} role={user.role} likes={user.stats.likes} posts={user.stats.posts} isFollowing={user.isFollowing} verified = {user.verified}/>
+    <div className='w-full h-full p-5 grid gap-4 
+                grid-cols-[repeat(auto-fill,minmax(240px,1fr))] 
+                justify-items-center '>
+      {Users.map((user,idx)=>(
+          <Card key={idx} image={user.avatar} name={user.name} role={user.role} likes={user.stats.likes} posts={user.stats.posts} isFollowing={user.isFollowing} verified = {user.verified}/>
       ))} 
     </div>
   )
--- cardtask/src/components/Card.jsx
+++ cardtask/src/components/Card.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const card = ({image,name,verified,role,likes,posts,isFollowing}) => {
 
+
+  const [follow, setfollow] = useState(isFollowing)
+
+  function btnClicked(e){
+    e.stopPropagation();  
+    setfollow(prev =>!prev)
+  }
  
+  const [verify, setverify] = useState(verified)
+
+  function imageClick(){
+    setverify(prev =>!prev)
+  }
 
   return (
-    <div className='w-60 h-80 overflow-hidden rounded-4xl relative shadow-xl shadow-black/30 hover:scale-105 transition'>
-              <div className='h-full bg-center bg-cover brightness-90' style={{backgroundImage:`url(${image})`}}>
+    <div  className='w-60 h-80 overflow-hidden rounded-4xl relative shadow-xl shadow-black/30 hover:scale-101 transition'>
+              <div onClick={imageClick} className='h-full bg-center bg-cover brightness-90' style={{backgroundImage:`url(${image})`}}>
 
                 <div className="absolute inset-0 z-10 bg-linear-to-b from via-white/10 to-black/90 " ></div>
 
 
               <div className=' w-full flex flex-col items-center absolute left-[50%] -translate-x-1/2 bottom-0 z-20'>
-                <div className='text-white flex'><h1 className='text-white'>{name}</h1> {verified?<span className='ml-1'> <i class="ri-verified-badge-fill text-emerald-400"></i></span>:''}</div>
+                <div className='text-white flex'><h1 className='text-white'>{name}</h1> {verify?<span className='ml-1'> <i className="ri-verified-badge-fill text-emerald-400"></i></span>:''}</div>
                 <div className='text-white'><p>{role}</p></div>
 
                 <div className='text-white flex items-center justify-between w-full p-3'>
-                  <div className='text-sm'><i class="ri-user-line"></i> {likes}</div>
-                  <div className='text-sm'><i class="ri-multi-image-line"></i> {posts}</div>
-                  {isFollowing?
+                  <div className='text-sm'><i className="ri-user-line"></i> {likes}</div>
+                  <div className='text-sm'><i className="ri-multi-image-line"></i> {posts}</div>
+                  {follow?
                   
-                  <button className='text-black bg-white hover:bg-green-500 hover:text-white transition pt-2 pb-2 pr-5 pl-5 rounded-3xl'>Follow <i class="ri-add-large-line"></i></button>
+                  (<button onClick={btnClicked} className=' cursor-pointer text-black bg-white hover:bg-green-500 hover:text-white transition pt-2 pb-2 pr-5 pl-5 rounded-3xl'>Follow <i className="ri-add-large-line"></i></button>)
 
                   :
                   
-                  <button className='text-green-500 border border-green-500 bg-green-100 hover:bg-green-500 hover:text-white transition pt-2 pb-2 pr-5 pl-5 rounded-3xl'>Following </button>}
+                  (<button onClick={btnClicked} className='  cursor-pointer text-green-500 border border-green-500 bg-green-100 hover:bg-green-500 hover:text-white transition pt-2 pb-2 pr-5 pl-5 rounded-3xl'>Following </button>)
+                  }
                 </div>
 
               </div>
